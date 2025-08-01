00d0a5a8efdd4b9d8ac7284b6e109270b56df9e6
cesar.tomita <cesar.jun.tomita@gmail.com>
2025-07-31 21:43:41 -0300

    30 Days of Pandas

---
+++ Study Plan/30 Days of Pandas/03-customers-who-never-order-183.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    
+    df = customers.merge(orders, how="left", left_on="id", right_on="customerId")
+
+    return  df[df["customerId"].isnull()][["name"]].rename(columns={"name": "Customers"})
